d5e4be8fe744c600e095edc301f73fc5cb5b527c
Lramos91 <lucas.ramosdavid@gmail.com>
2022-02-16 20:38:13 -0300

    Atividade de cadastro

---
+++ Atividade2_CadastroEvento.js
@@ -0,0 +1,27 @@
+console.log("Começar inscrição")
+
+let listadeeventos = ["evento1", "evento2", "evento3"] //vetor ou array
+if (listadeeventos = "evento1") {
+    console.log ("Evento existente")
+} else {
+    console.log ("Evento não encontrado")
+}
+let data = 25/02/2022
+if (data => 15/02/2022) {
+    console.log ("Data permitida, prosseguir")
+} else { 
+    console.log("Data invalida")
+}
+let idade = 30
+if (idade =>18) {
+    console.log ("Cadastro permitido")
+} else {
+    console.log ("Cadastro inválido, idade inferior á 18 anos")
+}
+let numerodeinscritos = 135
+if (numerodeinscritos <=100){
+    console.log ("Cadastro aprovado")
+} else {
+    console.log ("Cadastro invalido, excedeu numero de participantes")
+}
+
